b374a732866d5c745bebf97d4dfaa44df8a25023
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-06-28 01:19:52 +0530

    Create Day 3: Intro to Conditional Statements.cpp

---
+++ 30 Days of Code/Day 3: Intro to Conditional Statements.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int N;
+    cin >> N;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    if(N%2 == 1)
+    {
+        cout << "Weird";
+    }
+
+    if(N%2 == 0){ 
+        if (N>=2 && N<=5) 
+        {
+            cout << "Not Weird";
+        }
+
+        if (N<=20 && N>=6)
+        {
+            cout << "Weird";
+        }
+
+        if (N>20)
+        {
+            cout << "Not Weird";
+        }}
+
+    return 0;
+}
